866befa3e5bf196a89c1c520831fcdd3b85b6934
Base <41650303+airdropavi@users.noreply.github.com>
2026-03-09 03:16:37 +0300

    Create route.ts

---
+++ app/app/api/tx/route.ts
@@ -0,0 +1,23 @@
+import { FrameTransactionResponse } from '@coinbase/onchainkit/frame';
+import { NextResponse } from 'next/server';
+
+export async function POST(): Promise<NextResponse<FrameTransactionResponse>> {
+  return NextResponse.json({
+    chainId: 'eip155:8453',
+    method: 'eth_sendTransaction',
+    params: {
+      abi: [
+        {
+          "inputs": [{ "internalType": "string", "name": "_message", "type": "string" }],
+          "name": "dropMessage",
+          "outputs": [],
+          "stateMutability": "payable",
+          "type": "function"
+        }
+      ],
+      to: '0xf24aa53498f63216dd38d67702c5198f93cf00e7',
+      data: '', 
+      value: '100000000000000', // 0.0001 ETH
+    },
+  });
+}
