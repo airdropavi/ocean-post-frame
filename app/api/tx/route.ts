495e555e25764525665fd1ee9bb9e43170377050
Base <41650303+airdropavi@users.noreply.github.com>
2026-03-09 04:09:05 +0300

    Update route.ts

--- app/api/tx/route.ts
+++ app/api/tx/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function POST(): Promise<NextResponse<FrameTransactionResponse>> {
   return NextResponse.json({
-    chainId: 'eip155:8453',
+    chainId: 'eip155:8453', // Base Mainnet
     method: 'eth_sendTransaction',
     params: {
       abi: [
@@ -16,7 +16,7 @@ export async function POST(): Promise<NextResponse<FrameTransactionResponse>> {
         }
       ],
       to: '0xf24aa53498f63216dd38d67702c5198f93cf00e7',
-      data: '', 
+      data: '0x', // Boş tırnak yerine 0x yazdık, hata burada çözülüyor
       value: '100000000000000', // 0.0001 ETH
     },
   });
